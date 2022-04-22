4ad4ba9db12e4820378519d1fe6c8664d774c40d
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-04-22 11:36:30 -0700

    Started adding loops to 2b lab

--- Lesson 2/Lab/src/ca/bcit/comp2501/lab2b/monikaszucsparhammehrgan/Calendar.java
+++ Lesson 2/Lab/src/ca/bcit/comp2501/lab2b/monikaszucsparhammehrgan/Calendar.java
@@ -1,20 +1,62 @@
 package ca.bcit.comp2501.lab2b.monikaszucsparhammehrgan;
 
-import ca.bcit.comp2501.lab2a.monikaszucsparhammehrgan.BankAccount;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
+
 public class Calendar {
+
     private ArrayList<Integer> years;
-    private String[] month;
+    private String[] months;
     private ArrayList<Integer> days;
     HashMap<String, Date> dates;
 
-    public Calendar()
+    public Calendar(ArrayList<Integer> date)
     {
-        years = new ArrayList<>();
+        int yearStart = 1910;
+        int yearEnd = 2026;
+        years = new ArrayList<Integer>(yearStart);
+        for(int i = yearStart; i < yearEnd; i++){
+            years.add(i);
+        }
+        System.out.println(years);
 
-        years.add(1);
+        months = new String[12];
+        months[0] = "January";
+        months[1] = "February";
+        months[2] = "March";
+        months[3] = "April";
+        months[4] = "May";
+        months[5] = "June";
+        months[6] = "July";
+        months[7] = "August";
+        months[8] = "September";
+        months[9] = "October";
+        months[10] = "November";
+        months[11] = "December";
+
+        if(months != null)
+        {
+            for(int j = 0; j < months.length; j++)
+            {
+                if(months[j] != null)
+                {
+                    System.out.println(months[j]);
+                }
+            }
+        }
+
+        int dayStart = 1;
+        int dayEnd = 31;
+        days = new ArrayList<Integer>(dayStart);
+        for(int k = dayStart; k < dayEnd; k++){
+            days.add(k);
+        }
+        System.out.println(days);
+
+
     }
+
+
 }
--- Lesson 2/Lab/src/ca/bcit/comp2501/lab2b/monikaszucsparhammehrgan/Main.java
+++ Lesson 2/Lab/src/ca/bcit/comp2501/lab2b/monikaszucsparhammehrgan/Main.java
@@ -1,9 +1,18 @@
 package ca.bcit.comp2501.lab2b.monikaszucsparhammehrgan;
 
+import ca.bcit.comp2501.lab2a.monikaszucsparhammehrgan.Bank;
+import ca.bcit.comp2501.lab2a.monikaszucsparhammehrgan.BankAccount;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(final String[] args)
     {
-        Calendar event = new Calendar();
+        ArrayList<Integer> Date = null;
+        Calendar scheduled = new Calendar(Date);
+
+        Date event = new Date(2021, "December", 25);
+        System.out.println(event.getDate());
 
     }
 }

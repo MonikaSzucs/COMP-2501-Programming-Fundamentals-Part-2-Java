48e75415eb0d8fffd962e6bc72cb12c10e22f2e2
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-04-29 22:39:05 -0700

    Started setting up the Agency test. Still not done property test

---
+++ Lesson 3/2501assignment1practice/src/java/Agency.java
@@ -0,0 +1,20 @@
+public class Agency {
+    Object property;
+    Object agency;
+
+    public Agency(final Object property, final Object agency)
+    {
+        this.property = property;
+        this.agency = agency;
+    }
+
+    public Object getProperty()
+    {
+        return property;
+    }
+
+    public void addProperty(Object property)
+    {
+        this.property.property;
+    }
+}

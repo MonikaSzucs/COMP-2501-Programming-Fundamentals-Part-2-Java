d89dc4d941728c5ff8886d95aca9c9fa55f847f8
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-04-30 20:49:46 -0700

    Setup Lab8

---
+++ Lesson 8/Lab8/src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private final String firstName;
+    private final String lastName;
+    private final int birthYear;
+    private final double percentageGrade;
+    private final boolean pass;
+
+    public Student(final String firstName, final String lastName, final int birthYear, final double percentageGrade, final boolean pass)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.percentageGrade = percentageGrade;
+        this.pass = pass;
+    }
+
+    public static void main(final String[] args)
+    {
+
+    }
+}

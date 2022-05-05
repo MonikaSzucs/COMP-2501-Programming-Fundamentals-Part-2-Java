c0d46cd9dfef06012657e775698fbd9588ba56d0
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-04 17:17:34 -0700

    Loop within a Loop

--- Lesson 3/Example/src/Electronic.java
+++ Lesson 3/Example/src/Electronic.java
@@ -27,25 +27,51 @@ class Electronic {
         Electronic eletronics;
         eletronics = new Electronic();
 
-        List<List<String>> outer = new ArrayList<List<Integer>>();
-        List<String> inner1 = new ArrayList<Integer>();
-        List<String> inner2 = new ArrayList<Integer>();
+        List<List<String>> outer = new ArrayList<List<String>>();
+        List<String> inner1 = new ArrayList<String>();
+        List<String> inner2 = new ArrayList<String>();
+        List<String> inner3 = new ArrayList<String>();
 
         // Monitor 1
         inner1.add("OLED");
         inner1.add("ALIENWARE 34 CURVED QD-OLED GAMING MONITOR - AW3423DW");
-        inner1.add("1,");
+        inner1.add("1,600");
 
         // Monitor 2
-        inner2.add(300);
-        inner2.add(400);
+        inner2.add("LED");
+        inner2.add("Sceptre Curved 30\" 21:9 Gaming LED Monitor");
+        inner2.add("400");
+
+        // Monitor 3
+        inner3.add("QLED");
+        inner3.add("Curved Super Wide 32:9 Gaming Monitor");
+        inner3.add("1,300");
 
         // Monitor 3
         outer.add(inner1);
         outer.add(inner2);
+        outer.add(inner3);
 
-        outer.get(0).add(300);
+        //outer.get(0).add(300);
 
         System.out.println(outer);
+
+        if( outer != null)
+        {
+            for(List item: outer)
+            {
+                if(item != null)
+                {
+                    System.out.println(item);
+                    for(Object description: item)
+                    {
+                        if(description != null) {
+
+                            System.out.println(description);
+                        }
+                    }
+                }
+            }
+        }
     }
 }

ceebf3fe45ff40512c4379f62543a46c3e206c98
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-05 19:13:21 -0700

    Started lab 6, 7, and 8

--- Lesson 6/Lab6/src/StarWarsName.java
+++ Lesson 6/Lab6/src/StarWarsName.java
@@ -10,5 +10,10 @@ public class StarWarsName {
     public static void main(final String[] args)
     {
         String starWarsName = "Brady|Mills|Shepherd|Chilliwack";
+
+        String[] arrOfStr = starWarsName.split("\\|", 4);
+
+        for (String a : arrOfStr)
+            System.out.println(a);
     }
 }

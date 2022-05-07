019f5c36eb18e652708eaa2ed9c3e03424ff2488
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-06 18:32:48 -0700

    Started setting up lab 8

--- Lesson 8/Lab8/src/Lab8Driver.java
+++ Lesson 8/Lab8/src/Lab8Driver.java
@@ -1,2 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lab8Driver {
+    Map<String, String> students;
+
+    Lab8Driver(){
+        students = new HashMap<String,String>();
+    }
+
+    public void readFromPrompt()
+    {
+
+    }
+
+    public void readFromFile()
+    {
+
+    }
+
+    public void showStudents()
+    {
+        
+    }
+
+    public static void main(final String[] args)
+    {
+
+    }
 }
--- Lesson 8/Lab8/src/Student.java
+++ Lesson 8/Lab8/src/Student.java
@@ -1,17 +1,25 @@
 public class Student {
-    private final String firstName;
-    private final String lastName;
-    private final int birthYear;
-    private final double percentageGrade;
-    private final boolean pass;
+    private final String    firstName;
+    private final String    lastName;
+    private final String    idNumber;
+    private final int       birthYear;
+    private final double    percentageGrade;
+    private final boolean   pass;
 
-    public Student(final String firstName, final String lastName, final int birthYear, final double percentageGrade, final boolean pass)
+    public Student(final String firstName, final String lastName, final String idNumber, final int birthYear, final double percentageGrade)
     {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.idNumber = idNumber;
         this.birthYear = birthYear;
         this.percentageGrade = percentageGrade;
-        this.pass = pass;
+
+        if(percentageGrade >= 60) {
+            pass = true;
+        }
+        else {
+            pass = false;
+        }
     }
 
     public static void main(final String[] args)

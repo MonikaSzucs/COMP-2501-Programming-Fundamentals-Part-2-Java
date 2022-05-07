9e7f2a14ff2a7431dadbc13d1793d079aa16ff2b
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-06 18:45:03 -0700

    Setup more of lab 8

--- Lesson 8/Lab8/src/Lab8Driver.java
+++ Lesson 8/Lab8/src/Lab8Driver.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Lab8Driver {
     Map<String, String> students;
@@ -10,7 +11,16 @@ public class Lab8Driver {
 
     public void readFromPrompt()
     {
-
+        boolean proceed = true;
+        do {
+            System.out.println("Do you with to create a Student? (y/n: ");
+            String choice = scanner.next().toLowerCase();
+            if (choice.equals("y")) {
+                // here prompt for data
+            } else {
+                proceed = false;
+            }
+        } while (proceed);
     }
 
     public void readFromFile()
@@ -20,11 +30,12 @@ public class Lab8Driver {
 
     public void showStudents()
     {
-        
+
     }
 
     public static void main(final String[] args)
     {
-
+        Scanner scanner;
+        scanner = new Scanner(System.in);
     }
 }

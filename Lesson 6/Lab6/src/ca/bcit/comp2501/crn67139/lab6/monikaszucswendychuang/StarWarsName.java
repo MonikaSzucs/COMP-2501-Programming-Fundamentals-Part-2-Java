25cd4974e67e6ad5463f70dff4d7944939f6e592
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-22 16:55:30 -0700

    Fixed docstring notes for Java.
    Commented on IllegalArgument notes above the constructor and getter method.

--- Lesson 6/Lab6/src/ca/bcit/comp2501/crn67139/lab6/monikaszucswendychuang/StarWarsName.java
+++ Lesson 6/Lab6/src/ca/bcit/comp2501/crn67139/lab6/monikaszucswendychuang/StarWarsName.java
@@ -8,12 +8,12 @@ import java.util.Locale;
  * Wednesday evenings, Spring/Summer 2022
  * Lab #6
  *
- * Define a novel class and novels properties.
+ * Define a StarWarsName with StarWars properties.
  *
  * @author Monika Szucs
  * @author Wendy Chuang
  *
- * @version 1.2
+ * @version 1.3
  *
  * To run the StarWarsName you need to go to Run > Edit Configurations then under Build and Run program arguments
  * type in: Brady|Mills|Shepherd|Chilliwack
@@ -33,9 +33,15 @@ public class StarWarsName {
     public static final int INDEX_TWO     = 2;
     public static final int INDEX_THREE   = 3;
 
+    /**
+     * This is the Constructor that will first check if there are the right number of words given from the regex
+     *
+     * @param starWarsName this is the String of text given from the argument and is passed into the constructor
+     * @throws IllegalArgumentException this will return an Illegal Argument message if the user did not enter the
+     *                                  right number of arguments. It should be exactly 4.
+     */
     public StarWarsName(final String starWarsName)
     {
-        System.out.println(starWarsName.split("\\|").length);
         if(starWarsName.split("\\|").length != NUM_OF_WORD_STRINGS) {
             throw new IllegalArgumentException("You do not have 4 arguments to work with. Your have: " +
                     starWarsName.split("\\|").length);
@@ -43,12 +49,16 @@ public class StarWarsName {
         this.starWarsName = starWarsName;
     }
 
+    /**
+     * This is the getter which creates the generated StarWars name and outputs it for the user to see.
+     */
     public static void getStarWarsName() {
         String[] arrayNames;
         String nameSubstring1;
         String nameSubstring2;
         String nameSubstring3;
         String nameSubstring4;
+        StringBuilder nameBuilder;
         String generatedName;
 
         arrayNames = starWarsName.split("\\|");
@@ -58,7 +68,15 @@ public class StarWarsName {
         nameSubstring3 = String.valueOf(arrayNames[INDEX_TWO].toUpperCase().charAt(INDEX_ZERO) + arrayNames[INDEX_TWO].toLowerCase().substring(INDEX_ONE,INDEX_TWO));
         nameSubstring4 = String.valueOf(arrayNames[INDEX_THREE].toLowerCase().substring(INDEX_ZERO,INDEX_THREE));
 
-        generatedName = nameSubstring1 + nameSubstring2 + " " + nameSubstring3 + nameSubstring4;
+        nameBuilder = new StringBuilder();
+        nameBuilder.append(nameSubstring1);
+        nameBuilder.append(nameSubstring2);
+        nameBuilder.append(" ");
+        nameBuilder.append(nameSubstring3);
+        nameBuilder.append(nameSubstring4);
+
+        generatedName = nameBuilder.toString();
+
         System.out.format(generatedName);
     }
 
@@ -69,8 +87,8 @@ public class StarWarsName {
      * Brady|Mills|Shepherd|Chilliwack
      *
      * @param args grabbing the arguments provided to create the Star Wars name
-     * @ArrayIndexOutOfBoundsException this exception is used to check if the argument provided is out of bounds
-     *                                  then it will display the error
+     * @IllegalArgumentException  this exception is used to check if the argument provided is out of bounds. If it
+     *                              does not contain ONE argument then this exception will be thrown.
      */
     public static void main(final String[] args)
     {
